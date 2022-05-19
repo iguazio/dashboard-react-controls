74989587fedfc8c9d0236e9c2f48606c8c16ee51
ilan7empest <therecanbeonly1.1984@gmail.com>
2022-05-19 17:39:26 +0300

    revert dist changes

--- dist/components/index.js
+++ dist/components/index.js
@@ -9,18 +9,6 @@ Object.defineProperty(exports, "Button", {
     return _Button.default;
   }
 });
-Object.defineProperty(exports, "ConfirmDialog", {
-  enumerable: true,
-  get: function get() {
-    return _ConfirmDialog.default;
-  }
-});
-Object.defineProperty(exports, "Modal", {
-  enumerable: true,
-  get: function get() {
-    return _Modal.default;
-  }
-});
 Object.defineProperty(exports, "PopUpDialog", {
   enumerable: true,
   get: function get() {
@@ -45,18 +33,8 @@ Object.defineProperty(exports, "Tooltip", {
     return _Tooltip.default;
   }
 });
-Object.defineProperty(exports, "Wizard", {
-  enumerable: true,
-  get: function get() {
-    return _Wizard.default;
-  }
-});
 
 var _Button = _interopRequireDefault(require("./Button/Button"));
-
-var _ConfirmDialog = _interopRequireDefault(require("./ConfirmDialog/ConfirmDialog"));
-
-var _Modal = _interopRequireDefault(require("./Modal/Modal"));
 
 var _PopUpDialog = _interopRequireDefault(require("./PopUpDialog/PopUpDialog"));
 
@@ -66,6 +44,5 @@ var _TextTooltipTemplate = _interopRequireDefault(require("./TooltipTemplate/Tex
 
 var _Tooltip = _interopRequireDefault(require("./Tooltip/Tooltip"));
 
-var _Wizard = _interopRequireDefault(require("./Wizard/Wizard"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
--- dist/types.js
+++ dist/types.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.WIZARD_STEPS_CONFIG = exports.POP_UP_CUSTOM_POSITION = exports.MODAL_SIZES = exports.CONFIRM_DIALOG_BUTTON = exports.BUTTON_VARIANTS = void 0;
+exports.POP_UP_CUSTOM_POSITION = exports.BUTTON_VARIANTS = void 0;
 
 var _propTypes = _interopRequireDefault(require("prop-types"));
 
@@ -21,23 +21,3 @@ var POP_UP_CUSTOM_POSITION = _propTypes.default.shape({
 });
 
 exports.POP_UP_CUSTOM_POSITION = POP_UP_CUSTOM_POSITION;
-
-var MODAL_SIZES = _propTypes.default.oneOf([_constants.MODAL_SM, _constants.MODAL_MD, _constants.MODAL_LG]);
-
-exports.MODAL_SIZES = MODAL_SIZES;
-
-var CONFIRM_DIALOG_BUTTON = _propTypes.default.shape({
-  handler: _propTypes.default.func.isRequired,
-  label: _propTypes.default.string.isRequired,
-  variant: _propTypes.default.string.isRequired
-});
-
-exports.CONFIRM_DIALOG_BUTTON = CONFIRM_DIALOG_BUTTON;
-
-var WIZARD_STEPS_CONFIG = _propTypes.default.arrayOf(_propTypes.default.shape({
-  id: _propTypes.default.string,
-  label: _propTypes.default.string,
-  getActions: _propTypes.default.func
-}));
-
-exports.WIZARD_STEPS_CONFIG = WIZARD_STEPS_CONFIG;
