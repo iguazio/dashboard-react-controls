aa8c464eaeae70bfb1d22356a93e91a6380dc82b
mariana-furyk <mariana.furyk@globallogic.com>
2022-06-30 23:55:07 +0300

    Fix [Modal] on navigation back

--- src/lib/components/Modal/Modal.js
+++ src/lib/components/Modal/Modal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import { createPortal } from 'react-dom'
 import { CSSTransition } from 'react-transition-group'
@@ -14,8 +14,30 @@ import { ReactComponent as CloseIcon } from '../../images/close.svg'
 
 import './Modal.scss'
 
-const JSX_MODAL = ({ actions, children, className, onClose, size, show, title }) => {
+const JSX_MODAL = ({
+  actions,
+  children,
+  className,
+  location,
+  onClose,
+  onResolve,
+  size,
+  show,
+  title
+}) => {
+  const [currentLocation, setCurrentLocation] = useState(location)
   const modalClassNames = classNames('modal', className, size && `modal-${size}`)
+
+  useEffect(() => {
+    setCurrentLocation(location)
+
+    return () => {
+      if (location !== currentLocation) {
+        onResolve()
+      }
+    }
+  }, [currentLocation, location, onResolve])
+
   return (
     <>
       <Backdrop onClose={onClose} show={show} />
@@ -65,7 +87,9 @@ Modal.propTypes = {
     PropTypes.node,
     PropTypes.string
   ]).isRequired,
+  location: PropTypes.string.isRequired,
   onClose: PropTypes.func.isRequired,
+  onResolve: PropTypes.func.isRequired,
   show: PropTypes.bool.isRequired,
   size: MODAL_SIZES,
   title: PropTypes.string
