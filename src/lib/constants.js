9c9eebca3e703a136f9590791b77da4a7768b385
Ilank <63646693+ilan7empest@users.noreply.github.com>
2022-11-06 09:49:03 +0200

    Revert "Impl [UI] Manage Artifact tagging (#72)"
    This reverts commit 0cabacb85731f5d1e3bda8970c14c5ac6b8aef07.

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -58,5 +58,3 @@ export const FORBIDDEN_ERROR_STATUS_CODE = 403
 export const MODAL_SM = 'sm'
 export const MODAL_MD = 'md'
 export const MODAL_LG = 'lg'
-export const MODAL_MIN = 'min'
-export const MODAL_MAX = 'max'
--- src/lib/types.js
+++ src/lib/types.js
@@ -19,57 +19,61 @@ import PropTypes from 'prop-types'
 import {
   DANGER_BUTTON,
   LABEL_BUTTON,
-  MODAL_LG,
-  MODAL_MAX,
-  MODAL_MD,
-  MODAL_MIN,
   MODAL_SM,
+  MODAL_MD,
+  MODAL_LG,
   PRIMARY_BUTTON,
   SECONDARY_BUTTON,
   TERTIARY_BUTTON
 } from './constants'
 
 export const BUTTON_VARIANTS = PropTypes.oneOf([
-  DANGER_BUTTON,
-  LABEL_BUTTON,
   PRIMARY_BUTTON,
   SECONDARY_BUTTON,
-  TERTIARY_BUTTON
+  TERTIARY_BUTTON,
+  DANGER_BUTTON,
+  LABEL_BUTTON
 ])
 
 export const CHIP = PropTypes.shape({
-  delimiter: PropTypes.element,
-  id: PropTypes.string,
-  value: PropTypes.string.isRequired
+    delimiter: PropTypes.element,
+    id: PropTypes.string,
+    value: PropTypes.string.isRequired
 })
 
 export const CHIP_INPUT_LIST = PropTypes.arrayOf(
-  PropTypes.shape({
-    disabled: PropTypes.bool,
-    icon: PropTypes.element,
-    id: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-    subLabel: PropTypes.string,
-    ui: PropTypes.shape({})
-  })
+    PropTypes.shape({
+        disabled: PropTypes.bool,
+        icon: PropTypes.element,
+        id: PropTypes.string.isRequired,
+        label: PropTypes.string.isRequired,
+        subLabel: PropTypes.string,
+        ui: PropTypes.shape({})
+    })
 )
 
 export const CHIP_OPTIONS = PropTypes.shape({
-  background: PropTypes.oneOf([
-    'amethyst',
-    'green',
-    'grey',
-    'java',
-    'none',
-    'orange',
-    'purple',
-    'sorbus'
-  ]),
-  boldValue: PropTypes.bool,
-  borderColor: PropTypes.oneOf(['transparent', 'orange', 'green', 'purple', 'grey']),
-  density: PropTypes.oneOf(['dense', 'normal', 'medium']),
-  font: PropTypes.oneOf(['primary', 'white', 'green', 'purple', 'orange']),
-  borderRadius: PropTypes.oneOf(['primary', 'secondary'])
+    background: PropTypes.oneOf([
+        'none',
+        'orange',
+        'green',
+        'purple',
+        'grey',
+        'sorbus',
+        'java',
+        'amethyst'
+    ]),
+    boldValue: PropTypes.bool,
+    borderColor: PropTypes.oneOf([
+        'transparent',
+        'orange',
+        'green',
+        'purple',
+        'grey'
+    ]),
+    density: PropTypes.oneOf(['dense', 'normal', 'medium']),
+    font: PropTypes.oneOf(['primary', 'white', 'green', 'purple', 'orange']),
+    borderRadius: PropTypes.oneOf(['primary', 'secondary'])
 })
 
 export const CHIPS = PropTypes.arrayOf(CHIP)
@@ -79,7 +83,7 @@ export const POP_UP_CUSTOM_POSITION = PropTypes.shape({
   position: PropTypes.oneOf(['top-left', 'top-right', 'bottom-left', 'bottom-right'])
 })
 
-export const MODAL_SIZES = PropTypes.oneOf([MODAL_SM, MODAL_MD, MODAL_LG, MODAL_MIN, MODAL_MAX])
+export const MODAL_SIZES = PropTypes.oneOf([MODAL_SM, MODAL_MD, MODAL_LG])
 
 export const CONFIRM_DIALOG_CANCEL_BUTTON = PropTypes.shape({
   handler: PropTypes.func,
