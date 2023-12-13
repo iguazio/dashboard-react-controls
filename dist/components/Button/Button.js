f3b9fa3de5cb9e33a5214aff3184c96653e552d9
ilan7empest <therecanbeonly1.1984@gmail.com>
2023-12-13 20:50:18 +0700

    revert dist files

--- dist/components/Button/Button.js
+++ dist/components/Button/Button.js
@@ -1,20 +1,38 @@
-"use strict";
+'use strict'
 
-function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
-Object.defineProperty(exports, "__esModule", {
+function _typeof(obj) {
+  '@babel/helpers - typeof'
+  return (
+    (_typeof =
+      'function' == typeof Symbol && 'symbol' == typeof Symbol.iterator
+        ? function (obj) {
+            return typeof obj
+          }
+        : function (obj) {
+            return obj &&
+              'function' == typeof Symbol &&
+              obj.constructor === Symbol &&
+              obj !== Symbol.prototype
+              ? 'symbol'
+              : typeof obj
+          }),
+    _typeof(obj)
+  )
+}
+Object.defineProperty(exports, '__esModule', {
   value: true
-});
-exports.default = void 0;
-var _react = _interopRequireWildcard(require("react"));
-var _propTypes = _interopRequireDefault(require("prop-types"));
-var _classnames = _interopRequireDefault(require("classnames"));
-var _Tooltip = _interopRequireDefault(require("../Tooltip/Tooltip"));
-var _TextTooltipTemplate = _interopRequireDefault(require("../TooltipTemplate/TextTooltipTemplate"));
-var _types = require("../../types");
-var _constants = require("../../constants");
-require("./Button.scss");
-var _jsxRuntime = require("react/jsx-runtime");
-var _excluded = ["className", "density", "icon", "id", "label", "tooltip", "variant"];
+})
+exports.default = void 0
+var _react = _interopRequireWildcard(require('react'))
+var _propTypes = _interopRequireDefault(require('prop-types'))
+var _classnames = _interopRequireDefault(require('classnames'))
+var _Tooltip = _interopRequireDefault(require('../Tooltip/Tooltip'))
+var _TextTooltipTemplate = _interopRequireDefault(require('../TooltipTemplate/TextTooltipTemplate'))
+var _types = require('../../types')
+var _constants = require('../../constants')
+require('./Button.scss')
+var _jsxRuntime = require('react/jsx-runtime')
+var _excluded = ['className', 'density', 'icon', 'id', 'label', 'tooltip', 'variant']
 /*
 Copyright 2022 Iguazio Systems Ltd.
 Licensed under the Apache License, Version 2.0 (the "License") with
@@ -31,17 +49,129 @@ illegal under applicable law, and the grant of the foregoing license
 under the Apache 2.0 license is conditioned upon your compliance with
 such restriction.
 */
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-function _getRequireWildcardCache(nodeInterop) { if (typeof WeakMap !== "function") return null; var cacheBabelInterop = new WeakMap(); var cacheNodeInterop = new WeakMap(); return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) { return nodeInterop ? cacheNodeInterop : cacheBabelInterop; })(nodeInterop); }
-function _interopRequireWildcard(obj, nodeInterop) { if (!nodeInterop && obj && obj.__esModule) { return obj; } if (obj === null || _typeof(obj) !== "object" && typeof obj !== "function") { return { default: obj }; } var cache = _getRequireWildcardCache(nodeInterop); if (cache && cache.has(obj)) { return cache.get(obj); } var newObj = {}; var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor; for (var key in obj) { if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) { var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null; if (desc && (desc.get || desc.set)) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } newObj.default = obj; if (cache) { cache.set(obj, newObj); } return newObj; }
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }
-function _defineProperty(obj, key, value) { key = _toPropertyKey(key); if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
-function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-var Button = /*#__PURE__*/(0, _react.forwardRef)(function (_ref, ref) {
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj }
+}
+function _getRequireWildcardCache(nodeInterop) {
+  if (typeof WeakMap !== 'function') return null
+  var cacheBabelInterop = new WeakMap()
+  var cacheNodeInterop = new WeakMap()
+  return (_getRequireWildcardCache = function _getRequireWildcardCache(nodeInterop) {
+    return nodeInterop ? cacheNodeInterop : cacheBabelInterop
+  })(nodeInterop)
+}
+function _interopRequireWildcard(obj, nodeInterop) {
+  if (!nodeInterop && obj && obj.__esModule) {
+    return obj
+  }
+  if (obj === null || (_typeof(obj) !== 'object' && typeof obj !== 'function')) {
+    return { default: obj }
+  }
+  var cache = _getRequireWildcardCache(nodeInterop)
+  if (cache && cache.has(obj)) {
+    return cache.get(obj)
+  }
+  var newObj = {}
+  var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor
+  for (var key in obj) {
+    if (key !== 'default' && Object.prototype.hasOwnProperty.call(obj, key)) {
+      var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null
+      if (desc && (desc.get || desc.set)) {
+        Object.defineProperty(newObj, key, desc)
+      } else {
+        newObj[key] = obj[key]
+      }
+    }
+  }
+  newObj.default = obj
+  if (cache) {
+    cache.set(obj, newObj)
+  }
+  return newObj
+}
+function ownKeys(object, enumerableOnly) {
+  var keys = Object.keys(object)
+  if (Object.getOwnPropertySymbols) {
+    var symbols = Object.getOwnPropertySymbols(object)
+    enumerableOnly &&
+      (symbols = symbols.filter(function (sym) {
+        return Object.getOwnPropertyDescriptor(object, sym).enumerable
+      })),
+      keys.push.apply(keys, symbols)
+  }
+  return keys
+}
+function _objectSpread(target) {
+  for (var i = 1; i < arguments.length; i++) {
+    var source = null != arguments[i] ? arguments[i] : {}
+    i % 2
+      ? ownKeys(Object(source), !0).forEach(function (key) {
+          _defineProperty(target, key, source[key])
+        })
+      : Object.getOwnPropertyDescriptors
+      ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source))
+      : ownKeys(Object(source)).forEach(function (key) {
+          Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key))
+        })
+  }
+  return target
+}
+function _defineProperty(obj, key, value) {
+  key = _toPropertyKey(key)
+  if (key in obj) {
+    Object.defineProperty(obj, key, {
+      value: value,
+      enumerable: true,
+      configurable: true,
+      writable: true
+    })
+  } else {
+    obj[key] = value
+  }
+  return obj
+}
+function _toPropertyKey(arg) {
+  var key = _toPrimitive(arg, 'string')
+  return _typeof(key) === 'symbol' ? key : String(key)
+}
+function _toPrimitive(input, hint) {
+  if (_typeof(input) !== 'object' || input === null) return input
+  var prim = input[Symbol.toPrimitive]
+  if (prim !== undefined) {
+    var res = prim.call(input, hint || 'default')
+    if (_typeof(res) !== 'object') return res
+    throw new TypeError('@@toPrimitive must return a primitive value.')
+  }
+  return (hint === 'string' ? String : Number)(input)
+}
+function _objectWithoutProperties(source, excluded) {
+  if (source == null) return {}
+  var target = _objectWithoutPropertiesLoose(source, excluded)
+  var key, i
+  if (Object.getOwnPropertySymbols) {
+    var sourceSymbolKeys = Object.getOwnPropertySymbols(source)
+    for (i = 0; i < sourceSymbolKeys.length; i++) {
+      key = sourceSymbolKeys[i]
+      if (excluded.indexOf(key) >= 0) continue
+      if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue
+      target[key] = source[key]
+    }
+  }
+  return target
+}
+function _objectWithoutPropertiesLoose(source, excluded) {
+  if (source == null) return {}
+  var target = {}
+  var sourceKeys = Object.keys(source)
+  var key, i
+  for (i = 0; i < sourceKeys.length; i++) {
+    key = sourceKeys[i]
+    if (excluded.indexOf(key) >= 0) continue
+    target[key] = source[key]
+  }
+  return target
+}
+var Button = /*#__PURE__*/ (0, _react.forwardRef)(function (_ref, ref) {
   var className = _ref.className,
     density = _ref.density,
     icon = _ref.icon,
@@ -49,24 +179,41 @@ var Button = /*#__PURE__*/(0, _react.forwardRef)(function (_ref, ref) {
     label = _ref.label,
     tooltip = _ref.tooltip,
     variant = _ref.variant,
-    restProps = _objectWithoutProperties(_ref, _excluded);
-  var buttonClassName = (0, _classnames.default)('btn', "btn-".concat(variant), "btn-".concat(density), className);
-  return /*#__PURE__*/(0, _jsxRuntime.jsxs)("button", _objectSpread(_objectSpread({}, restProps), {}, {
-    className: buttonClassName,
-    ref: ref,
-    "data-testid": id,
-    children: [icon, tooltip ? /*#__PURE__*/(0, _jsxRuntime.jsx)(_Tooltip.default, {
-      template: /*#__PURE__*/(0, _jsxRuntime.jsx)(_TextTooltipTemplate.default, {
-        text: tooltip
-      }),
-      children: /*#__PURE__*/(0, _jsxRuntime.jsx)("span", {
-        children: label
-      })
-    }) : /*#__PURE__*/(0, _jsxRuntime.jsx)("div", {
-      children: label
-    })]
-  }));
-});
+    restProps = _objectWithoutProperties(_ref, _excluded)
+  var buttonClassName = (0, _classnames.default)(
+    'btn',
+    'btn-'.concat(variant),
+    'btn-'.concat(density),
+    className
+  )
+  return /*#__PURE__*/ (0, _jsxRuntime.jsxs)(
+    'button',
+    _objectSpread(
+      _objectSpread({}, restProps),
+      {},
+      {
+        className: buttonClassName,
+        ref: ref,
+        'data-testid': id,
+        children: [
+          icon,
+          tooltip
+            ? /*#__PURE__*/ (0, _jsxRuntime.jsx)(_Tooltip.default, {
+                template: /*#__PURE__*/ (0, _jsxRuntime.jsx)(_TextTooltipTemplate.default, {
+                  text: tooltip
+                }),
+                children: /*#__PURE__*/ (0, _jsxRuntime.jsx)('span', {
+                  children: label
+                })
+              })
+            : /*#__PURE__*/ (0, _jsxRuntime.jsx)('span', {
+                children: label
+              })
+        ]
+      }
+    )
+  )
+})
 Button.defaultProps = {
   className: '',
   density: 'normal',
@@ -74,7 +221,7 @@ Button.defaultProps = {
   label: 'Button',
   tooltip: '',
   variant: _constants.TERTIARY_BUTTON
-};
+}
 Button.propTypes = {
   className: _propTypes.default.string,
   density: _propTypes.default.oneOf(['dense', 'normal', 'medium', 'chunky']),
@@ -83,6 +230,6 @@ Button.propTypes = {
   label: _propTypes.default.oneOfType([_propTypes.default.string, _propTypes.default.element]),
   tooltip: _propTypes.default.oneOfType([_propTypes.default.string, _propTypes.default.element]),
   variant: _types.BUTTON_VARIANTS
-};
-var _default = Button;
-exports.default = _default;
+}
+var _default = Button
+exports.default = _default
