e4707074bdaec69ddbbb4496a0046a968e5ddfc0
Ilank <63646693+ilan7empest@users.noreply.github.com>
2025-08-21 08:20:13 +0300

    Create a new dist version `3.1.7` (#433)
    Collecting:
    - Fix [SecVul] Fix Workflow does not contain permissions (#429)
    - Fix [SecVul] [3.7.1] Fix Unpinned tag for a non-immutable Action in workflow (#430)
    - Fix [Projects Setting] Redundant request for empty label data and invalid empty-key in Node Selector params (#431)
    - Fix [General] Breadcrumbs are scrolled with the content (#432)

--- dist/utils/form.util.mjs
+++ dist/utils/form.util.mjs
@@ -1,10 +1,10 @@
-import { set as u, isEqual as l, mapValues as d, some as m, get as c, isNil as o, isEmpty as f } from "lodash";
-const p = (e, t) => {
+import { set as u, isEqual as l, mapValues as m, some as c, get as d, isNil as o, isEmpty as y } from "lodash";
+const b = (e, t) => {
   let r = e[0], i = e[1], n = t.fields[r];
   if (n)
     for (let s in i)
       u(n, s, i[s]);
-}, b = (e, t) => {
+}, O = (e, t) => {
   const r = (i, n) => {
     if (n !== "")
       return n;
@@ -13,17 +13,18 @@ const p = (e, t) => {
     JSON.stringify(a(e), r),
     JSON.stringify(a(t), r)
   );
-}, O = (e = []) => e.reduce((t, r) => (t[r.data.key] = r.data.value, t), {}), g = (e = {}) => Object.entries(e).map(([t, r]) => ({
+}, F = (e = []) => e.reduce((t, r) => (t[r.data.key] = r.data.value, t), {}), g = (e = {}) => Object.entries(e).map(([t, r]) => ({
   data: {
     key: t,
     value: r
   }
-})), F = (e) => e.submitting || e.invalid && e.submitFailed, a = (e = {}) => d(e, (t) => Array.isArray(t) ? t.filter((r) => m(c(r, "data", r), (i) => !o(i) && !f(i))) : t);
+})), E = (e) => e.submitting || e.invalid && e.submitFailed, a = (e = {}) => m(e, (t) => Array.isArray(t) ? f(t) : t), f = (e = []) => e.filter((t) => c(d(t, "data", t), (r) => !o(r) && !y(r)));
 export {
-  b as areFormValuesChanged,
-  O as generateObjectFromKeyValue,
-  F as isSubmitDisabled,
+  O as areFormValuesChanged,
+  f as clearArrayFromEmptyObjectElements,
+  F as generateObjectFromKeyValue,
+  E as isSubmitDisabled,
   g as parseObjectToKeyValue,
-  p as setFieldState
+  b as setFieldState
 };
 //# sourceMappingURL=form.util.mjs.map
