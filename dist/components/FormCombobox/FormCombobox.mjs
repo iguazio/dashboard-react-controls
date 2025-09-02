06bc3634b80c453a40d5341d69c37ac3b8e6c441
Ilank <63646693+ilan7empest@users.noreply.github.com>
2025-09-02 21:21:55 +0800

    Create a new dist version `3.1.9` (#437)
    Collecting:
    - Fix [Batch run] small issues in the Data inputs "Path" field (#436)

--- dist/components/FormCombobox/FormCombobox.mjs
+++ dist/components/FormCombobox/FormCombobox.mjs
@@ -1,17 +1,17 @@
-import { jsx as l, jsxs as u } from "react/jsx-runtime";
-import { useState as c, useRef as R, useEffect as C, useCallback as K } from "react";
+import { jsx as t, jsxs as u } from "react/jsx-runtime";
+import { useState as d, useRef as R, useEffect as C, useCallback as K } from "react";
 import { useField as Ee, Field as Oe } from "react-final-form";
 import { isEmpty as q } from "lodash";
-import t from "prop-types";
-import h from "classnames";
-import ke from "../../elements/OptionsMenu/OptionsMenu.mjs";
-import Re from "../../elements/ValidationTemplate/ValidationTemplate.mjs";
+import i from "prop-types";
+import _ from "classnames";
+import Te from "../../elements/OptionsMenu/OptionsMenu.mjs";
+import ke from "../../elements/ValidationTemplate/ValidationTemplate.mjs";
 import Q from "../PopUpDialog/PopUpDialog.mjs";
-import Te from "../TooltipTemplate/TextTooltipTemplate.mjs";
+import Re from "../TooltipTemplate/TextTooltipTemplate.mjs";
 import Fe from "../Tooltip/Tooltip.mjs";
-import { checkPatternsValidity as De } from "../../utils/validation.util.mjs";
+import { checkPatternsValidity as $e } from "../../utils/validation.util.mjs";
 import "../../hooks/index.mjs";
-import { COMBOBOX_SUGGESTION_LIST as Le, COMBOBOX_SELECT_OPTIONS as $e, DENSITY as Ae } from "../../types.mjs";
+import { COMBOBOX_SUGGESTION_LIST as De, COMBOBOX_SELECT_OPTIONS as Le, DENSITY as Ae } from "../../types.mjs";
 import Pe from "../../images/arrow.svg.mjs";
 import je from "../../images/search.svg.mjs";
 import Me from "../../images/warning.svg.mjs";
@@ -23,16 +23,16 @@ const Be = ({
   density: ee = "normal",
   disabled: W = !1,
   hideSearchInput: oe = !1,
-  inputDefaultValue: le = "",
-  inputPlaceholder: ie = "",
+  inputDefaultValue: te = "",
+  inputPlaceholder: le = "",
   invalidText: B = "Invalid",
   label: G = "",
-  maxSuggestedMatches: te = 1,
-  name: m,
-  onBlur: _ = null,
-  onChange: g = null,
+  maxSuggestedMatches: ie = 1,
+  name: f,
+  onBlur: g = null,
+  onChange: w = null,
   onFocus: N = null,
-  required: T = !1,
+  required: F = !1,
   rules: H = [],
   selectDefaultValue: re = {
     label: "",
@@ -45,16 +45,16 @@ const Be = ({
   validator: U = null,
   withoutBorder: ae = !1
 }) => {
-  const { input: a, meta: r } = Ee(m), [F, D] = c(le), [n, ne] = c(re), [ce, L] = c({
+  const { input: a, meta: r } = Ee(f), [$, D] = d(te), [n, ne] = d(re), [ce, y] = d({
     left: "0px"
-  }), [p, f] = c(!1), [$, w] = c(!1), [x, X] = c(S), [v, y] = c(!1), [I, de] = c(!1), [V, me] = c(H), [A, E] = c(!1), b = R(), P = R(), d = R(), O = R();
-  We(b, () => E(!1));
-  const fe = h("form-field__label", W && "form-field__label-disabled"), ue = h(
+  }), [p, m] = d(!1), [L, b] = d(!1), [x, X] = d(S), [v, I] = d(!1), [V, de] = d(!1), [E, fe] = d(H), [A, O] = d(!1), h = R(), P = R(), c = R(), T = R();
+  We(h, () => O(!1));
+  const me = _("form-field__label", W && "form-field__label-disabled"), ue = _(
     "form-field-combobox__input",
     n.id.length === 0 && "form-field-combobox__input_hidden"
   );
   C(() => {
-    me(
+    fe(
       (e) => e.map((o) => ({
         ...o,
         isValid: !r.error || !Array.isArray(r.error) ? !0 : !r.error.some((s) => s.name === o.name)
@@ -69,86 +69,90 @@ const Be = ({
   }, [r.invalid, r.modified, r.submitFailed, r.touched, r.validating]);
   const j = K(
     (e) => {
-      b.current && !b.current.contains(e.target) && O.current && !O.current.contains(e.target) && (y(!1), f(!1), w(!1), a.onBlur(new Event("blur")), _ && _(a.value));
+      h.current && !h.current.contains(e.target) && T.current && !T.current.contains(e.target) && (I(!1), m(!1), b(!1), a.onBlur(new Event("blur")), g && g(a.value));
     },
-    [a, _]
+    [a, g]
   ), Y = (e) => {
-    b.current && b.current.contains(e.target) || !e.target.closest(".pop-up-dialog") && !e.target.classList.contains("form-field-combobox") && (E(!1), f(!1), w(!1), d.current.blur());
+    h.current && h.current.contains(e.target) || !e.target.closest(".pop-up-dialog") && !e.target.classList.contains("form-field-combobox") && (O(!1), m(!1), b(!1), c.current.blur());
   };
   C(() => (window.addEventListener("click", j), () => {
     window.removeEventListener("click", j);
-  }), [j]), C(() => ((A || p || $) && window.addEventListener("scroll", Y, !0), () => {
+  }), [j]), C(() => ((A || p || L) && window.addEventListener("scroll", Y, !0), () => {
     window.removeEventListener("scroll", Y, !0);
-  }), [p, $, A]);
-  const pe = () => V.map(({ isValid: e = !1, label: o, name: s }) => /* @__PURE__ */ l(Re, { valid: e, validationMessage: o }, s)), be = (e) => {
+  }), [p, L, A]);
+  const pe = () => E.map(({ isValid: e = !1, label: o, name: s }) => /* @__PURE__ */ t(ke, { valid: e, validationMessage: o }, s)), be = (e) => {
     const o = e.target;
-    L({
+    y({
       left: `${o.selectionStart < 30 ? o.selectionStart : 30}ch`
-    }), v && y(!1), D(o.value), a.onChange(`${n.id}${o.value}`), g && g(n.id, o.value), x.length > 0 && w(!0);
+    }), v && I(!1), D(o.value), a.onChange(`${n.id}${o.value}`), w && w(n.id, o.value), x.length > 0 && b(!0);
   }, he = (e) => {
-    e.id !== n.id ? (ne(e), a.onChange(e.id), D(""), g && g(e.id), f(!1), d.current.disabled = !1, d.current.focus()) : (f(!1), d.current.disabled = !1, d.current.focus());
+    e.id !== n.id ? (ne(e), a.onChange(e.id), D(""), w && w(e.id), m(!1), c.current.disabled = !1, c.current.focus()) : (m(!1), c.current.disabled = !1, c.current.focus());
   }, _e = (e) => {
-    const o = F.split("/"), s = o.length - 1;
-    let i = e.customDelimiter ? o[s].replace(new RegExp(`${e.customDelimiter}.*`), "") + e.id : e.id;
-    o.length <= te - 1 && (i += "/"), o[s] = i, v && y(!1), o.join("/") !== F && (D(o.join("/")), a.onChange(`${n.id}${o.join("/")}`), g && g(n.id, o.join("/"))), w(!1), d.current.focus(), L({
-      left: `${d.current.selectionStart < 30 ? d.current.selectionStart : 30}ch`
+    const o = $.split("/"), s = o.length - 1;
+    let l = e.customDelimiter ? o[s].replace(new RegExp(`${e.customDelimiter}.*`), "") + e.id : e.id;
+    o.length <= ie - 1 && (l += "/"), o[s] = l, v && I(!1), o.join("/") !== $ && (D(o.join("/")), a.onChange(`${n.id}${o.join("/")}`), w && w(n.id, o.join("/"))), b(!1), c.current.focus(), y({
+      left: `${c.current.selectionStart < 30 ? c.current.selectionStart : 30}ch`
     });
-  }, ge = () => {
-    N && N(), a.onFocus(new Event("focus")), p && f(!1), w(!0);
+  }, ge = (e) => {
+    N && N(), a.onFocus(new Event("focus")), p && m(!1), c.current.selectionStart ? b(!0) : setTimeout(() => {
+      y({
+        left: `${e.target.selectionStart < 30 ? e.target.selectionStart : 30}ch`
+      }), b(!0);
+    });
   }, we = (e) => {
     e.persist(), X(
       () => S.filter((o) => o.id.startsWith(e.target.value))
     );
   }, z = K(() => {
-    p ? (f(!1), a.onBlur(new Event("blur")), _ && _(a.value)) : (w(!1), E(!1), L({
+    p ? (m(!1), a.onBlur(new Event("blur")), g && g(a.value)) : (b(!1), O(!1), y({
       left: "0px"
-    }), f(!0), a.onFocus(new Event("focus")), N && N(a.value));
-  }, [a, _, N, p]), xe = (e = "", o) => {
+    }), m(!0), a.onFocus(new Event("focus")), N && N(a.value));
+  }, [a, g, N, p]), xe = (e = "", o) => {
     const s = e.startsWith(n.id) ? e.substring(n.id.length) : e ?? "";
-    let i = null;
-    if (!q(V)) {
-      const [M, Ie] = De(H, s), Ve = M.filter((k) => !k.isValid);
-      Ie || (i = Ve.map((k) => ({ name: k.name, label: k.label })));
+    let l = null;
+    if (!q(E)) {
+      const [M, Ie] = $e(H, s), Ve = M.filter((k) => !k.isValid);
+      Ie || (l = Ve.map((k) => ({ name: k.name, label: k.label })));
     }
-    return q(i) && (s.startsWith(" ") ? i = { name: "empty", label: B } : T && s.trim().length === 0 && (i = { name: "required", label: "This field is required" })), !i && U && (i = U(e, o)), i;
+    return q(l) && (s.startsWith(" ") ? l = { name: "empty", label: B } : F && s.trim().length === 0 && (l = { name: "required", label: "This field is required" })), !l && U && (l = U(e, o)), l;
   }, ve = () => {
-    E((e) => !e), f(!1);
-  }, Ne = h(
+    O((e) => !e), m(!1);
+  }, Ne = _(
     Z,
     "form-field-combobox",
     "form-field",
-    I && "form-field-combobox_invalid"
-  ), Se = h(
+    V && "form-field-combobox_invalid"
+  ), Se = _(
     p && "form-field-combobox__icon_open",
     "form-field-combobox__icon"
-  ), Ce = h(n.className), ye = h(
+  ), Ce = _(n.className), ye = _(
     "form-field__wrapper",
     `form-field__wrapper-${ee}`,
     W && "form-field__wrapper-disabled",
-    I && "form-field__wrapper-invalid",
+    V && "form-field__wrapper-invalid",
     ae && "without-border"
   );
-  return /* @__PURE__ */ l(Oe, { name: m, validate: xe, children: ({ input: e, meta: o }) => {
+  return /* @__PURE__ */ t(Oe, { name: f, validate: xe, children: ({ input: e, meta: o }) => {
     var s;
     return /* @__PURE__ */ u(
       "div",
       {
         className: Ne,
-        ref: b,
-        "data-testid": m ? `${m}-form-combobox` : "form-combobox",
+        ref: h,
+        "data-testid": f ? `${f}-form-combobox` : "form-combobox",
         children: [
-          G && /* @__PURE__ */ l("div", { className: fe, children: /* @__PURE__ */ u("label", { "data-testid": "label", htmlFor: e.name, children: [
+          G && /* @__PURE__ */ t("div", { className: me, children: /* @__PURE__ */ u("label", { "data-testid": "label", htmlFor: e.name, children: [
             G,
-            (T || V.find((i) => i.name === "required")) && /* @__PURE__ */ l("span", { className: "form-field__label-mandatory", children: " *" })
+            (F || E.find((l) => l.name === "required")) && /* @__PURE__ */ t("span", { className: "form-field__label-mandatory", children: " *" })
           ] }) }),
           /* @__PURE__ */ u("div", { className: ye, children: [
-            /* @__PURE__ */ l("div", { className: "form-field__icons", children: /* @__PURE__ */ l(Pe, { className: Se, onClick: z }) }),
+            /* @__PURE__ */ t("div", { className: "form-field__icons", children: /* @__PURE__ */ t(Pe, { className: Se, onClick: z }) }),
             /* @__PURE__ */ u("div", { className: "form-field-combobox__select form-field__control", ref: P, children: [
               /* @__PURE__ */ u("div", { className: "form-field-combobox__select-header", onClick: z, children: [
-                /* @__PURE__ */ l("span", { className: Ce, children: n.id }),
-                n.id.length === 0 && J && /* @__PURE__ */ l("div", { className: "form-field-combobox__placeholder", children: /* @__PURE__ */ l("label", { children: J }) })
+                /* @__PURE__ */ t("span", { className: Ce, children: n.id }),
+                n.id.length === 0 && J && /* @__PURE__ */ t("div", { className: "form-field-combobox__placeholder", children: /* @__PURE__ */ t("label", { children: J }) })
               ] }),
-              p && /* @__PURE__ */ l(
+              p && /* @__PURE__ */ t(
                 Q,
                 {
                   headerIsHidden: !0,
@@ -157,42 +161,43 @@ const Be = ({
                     position: "bottom-right"
                   },
                   className: "form-field-combobox__dropdown form-field-combobox__dropdown-select",
-                  children: /* @__PURE__ */ l("ul", { className: "form-field-combobox__dropdown-list", ref: O, children: se.map((i) => {
-                    if (!i.hidden) {
-                      const M = h(
+                  children: /* @__PURE__ */ t("ul", { className: "form-field-combobox__dropdown-list", ref: T, children: se.map((l) => {
+                    if (!l.hidden) {
+                      const M = _(
                         "form-field-combobox__dropdown-list-option",
-                        i.className
+                        l.className
                       );
-                      return /* @__PURE__ */ l(
+                      return /* @__PURE__ */ t(
                         "li",
                         {
                           className: M,
-                          onClick: () => he(i),
-                          children: i.label
+                          onClick: () => he(l),
+                          children: l.label
                         },
-                        i.id
+                        l.id
                       );
                     }
                   }) })
                 }
               )
             ] }),
-            /* @__PURE__ */ l(
+            /* @__PURE__ */ t(
               "input",
               {
+                autoComplete: "off",
                 className: ue,
-                "data-testid": m ? `${m}-form-combobox-input` : "form-combobox-input",
+                "data-testid": f ? `${f}-form-combobox-input` : "form-combobox-input",
                 id: e.name,
                 onChange: be,
                 onFocus: ge,
-                placeholder: ie,
-                ref: d,
-                required: T,
+                placeholder: le,
+                ref: c,
+                required: F,
                 type: "text",
-                value: F
+                value: $
               }
             ),
-            $ && (x.length > 0 || v) && /* @__PURE__ */ l(
+            L && (x.length > 0 || v) && /* @__PURE__ */ t(
               Q,
               {
                 headerIsHidden: !0,
@@ -204,45 +209,46 @@ const Be = ({
                 style: {
                   ...ce
                 },
-                children: /* @__PURE__ */ u("div", { ref: O, children: [
+                children: /* @__PURE__ */ u("div", { ref: T, children: [
                   !oe && /* @__PURE__ */ u("div", { className: "form-field-combobox__search-wrapper", children: [
-                    /* @__PURE__ */ l(
+                    /* @__PURE__ */ t(
                       "input",
                       {
-                        "data-testid": m ? `${m}-form-combobox-search` : "form-combobox-search",
+                        autoComplete: "off",
+                        "data-testid": f ? `${f}-form-combobox-search` : "form-combobox-search",
                         className: "form-field-combobox__search form-field__control",
                         onChange: we,
-                        onFocus: () => y(!0),
+                        onFocus: () => I(!0),
                         placeholder: "Type to search",
                         type: "text"
                       }
                     ),
-                    /* @__PURE__ */ l(je, {})
+                    /* @__PURE__ */ t(je, {})
                   ] }),
-                  /* @__PURE__ */ l("ul", { className: "form-field-combobox__dropdown-list", children: v && x.length === 0 ? /* @__PURE__ */ l("li", { className: "form-field-combobox__dropdown-list-option", children: "No data" }, "no data") : x.map((i) => /* @__PURE__ */ l(
+                  /* @__PURE__ */ t("ul", { className: "form-field-combobox__dropdown-list", children: v && x.length === 0 ? /* @__PURE__ */ t("li", { className: "form-field-combobox__dropdown-list-option", children: "No data" }, "no data") : x.map((l) => /* @__PURE__ */ t(
                     "li",
                     {
                       className: "form-field-combobox__dropdown-list-option",
-                      onClick: () => _e(i),
-                      children: i.label
+                      onClick: () => _e(l),
+                      children: l.label
                     },
-                    i.id
+                    l.id
                   )) })
                 ] })
               }
             ),
             /* @__PURE__ */ u("div", { className: "form-field__icons", children: [
-              I && !Array.isArray(o.error) && /* @__PURE__ */ l(
+              V && !Array.isArray(o.error) && /* @__PURE__ */ t(
                 Fe,
                 {
                   className: "form-field__warning",
-                  template: /* @__PURE__ */ l(Te, { text: ((s = o.error) == null ? void 0 : s.label) ?? B, warning: !0 }),
-                  children: /* @__PURE__ */ l(qe, {})
+                  template: /* @__PURE__ */ t(Re, { text: ((s = o.error) == null ? void 0 : s.label) ?? B, warning: !0 }),
+                  children: /* @__PURE__ */ t(qe, {})
                 }
               ),
-              I && Array.isArray(o.error) && /* @__PURE__ */ l("button", { className: "form-field__warning", onClick: ve, children: /* @__PURE__ */ l(Me, {}) })
+              V && Array.isArray(o.error) && /* @__PURE__ */ t("button", { className: "form-field__warning", onClick: ve, children: /* @__PURE__ */ t(Me, {}) })
             ] }),
-            !q(V) && /* @__PURE__ */ l(ke, { show: A, ref: { refInputContainer: b }, children: pe() })
+            !q(E) && /* @__PURE__ */ t(Te, { show: A, ref: { refInputContainer: h }, children: pe() })
           ] })
         ]
       }
@@ -250,27 +256,27 @@ const Be = ({
   } });
 };
 Be.propTypes = {
-  comboboxClassName: t.string,
+  comboboxClassName: i.string,
   density: Ae,
-  disabled: t.bool,
-  hideSearchInput: t.bool,
-  inputDefaultValue: t.string,
-  inputPlaceholder: t.string,
-  invalidText: t.string,
-  label: t.string,
-  maxSuggestedMatches: t.number,
-  name: t.string.isRequired,
-  onBlur: t.func,
-  onChange: t.func,
-  onFocus: t.func,
-  required: t.bool,
-  rules: t.array,
-  selectDefaultValue: t.shape({}),
-  selectOptions: $e.isRequired,
-  selectPlaceholder: t.string,
-  suggestionList: Le,
-  validator: t.func,
-  withoutBorder: t.bool
+  disabled: i.bool,
+  hideSearchInput: i.bool,
+  inputDefaultValue: i.string,
+  inputPlaceholder: i.string,
+  invalidText: i.string,
+  label: i.string,
+  maxSuggestedMatches: i.number,
+  name: i.string.isRequired,
+  onBlur: i.func,
+  onChange: i.func,
+  onFocus: i.func,
+  required: i.bool,
+  rules: i.array,
+  selectDefaultValue: i.shape({}),
+  selectOptions: Le.isRequired,
+  selectPlaceholder: i.string,
+  suggestionList: De,
+  validator: i.func,
+  withoutBorder: i.bool
 };
 export {
   Be as default
