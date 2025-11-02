fca79d76b0463a3e01608222f7c44ab534d0a318
ilan7empest <therecanbeonly1.1984@gmail.com>
2025-11-02 10:28:10 +0200

    Create a new dist version 3.2.1
    Collecting:
    - Impl [Infrastructure] Integrating IGZ-4 via Module Federation (#447)

--- dist/utils/index.mjs
+++ dist/utils/index.mjs
@@ -1,25 +1,27 @@
 import * as t from "./chips.util.mjs";
-import * as o from "./common.util.mjs";
-import * as r from "./datetime.util.mjs";
-import * as i from "./filter.util.mjs";
-import * as a from "./form.util.mjs";
+import * as r from "./common.util.mjs";
+import * as o from "./proxyServerConfig.util.mjs";
+import * as i from "./datetime.util.mjs";
+import * as a from "./filter.util.mjs";
+import * as e from "./form.util.mjs";
 import * as m from "./generateChipsList.util.mjs";
 import * as s from "./getFirstScrollableParent.util.mjs";
-import * as e from "./math.util.mjs";
-import * as p from "./notification.util.mjs";
-import * as l from "./string.util.mjs";
-import * as f from "./validation.util.mjs";
+import * as p from "./math.util.mjs";
+import * as l from "./notification.util.mjs";
+import * as f from "./string.util.mjs";
+import * as n from "./validation.util.mjs";
 export {
   t as chips,
-  o as common,
-  r as datetime,
-  i as filter,
-  a as form,
+  r as common,
+  o as createProxy,
+  i as datetime,
+  a as filter,
+  e as form,
   m as generateChipsList,
   s as getFirstScrollableParent,
-  e as math,
-  p as notification,
-  l as string,
-  f as validation
+  p as math,
+  l as notification,
+  f as string,
+  n as validation
 };
 //# sourceMappingURL=index.mjs.map
