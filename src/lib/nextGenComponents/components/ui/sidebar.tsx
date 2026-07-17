7b64ca00c2f1dd027830f66a837c822cacf572b0
Taras Hlukhovetskyi <Taras_Bohdanovych_Hlukhovetskyi@external.mckinsey.com>
2026-07-17 17:04:54 +0300

    improvement

--- src/lib/nextGenComponents/components/ui/sidebar.tsx
+++ src/lib/nextGenComponents/components/ui/sidebar.tsx
@@ -265,29 +265,27 @@ const Sidebar = React.forwardRef<HTMLElement, SidebarProps>(
             className="flex size-full flex-col bg-sidebar shadow-[1px_4px_16px_rgba(0,0,0,0.04)] group-data-[variant=floating]:rounded-lg group-data-[variant=floating]:border group-data-[variant=floating]:border-sidebar-border group-data-[variant=floating]:shadow"
           >
             {children}
-            <Button
-              variant="outline"
-              tooltip={pinned ? 'Unpin sidebar' : 'Pin sidebar'}
-              data-testid="pin-sidebar-button"
-              data-sidebar="pin"
-              aria-hidden={!open}
-              tabIndex={open ? 0 : -1}
-              side="right"
-              className={cn(
-                'absolute top-2 left-full border bg-[#FAFAFA] border-gray-200 border-solid',
-                'w-fit h-fit rounded-l-none border-l-0 py-2 pr-[3px] pl-[1px]',
-                'transition-visibility hover:bg-sidebar-accent hover:text-sidebar-accent-foreground',
-
-                !open && 'hidden'
-              )}
-              onClick={togglePin}
-            >
-              {pinned ? (
-                <SidebarClose data-testid="closeed-pin" />
-              ) : (
-                <SidebarOpen data-testid="opened-pin" />
-              )}
-            </Button>
+            {open && (
+              <Button
+                variant="outline"
+                tooltip={pinned ? 'Unpin sidebar' : 'Pin sidebar'}
+                data-testid="pin-sidebar-button"
+                data-sidebar="pin"
+                side="right"
+                className={cn(
+                  'absolute top-2 left-full border bg-[#FAFAFA] border-gray-200 border-solid',
+                  'w-fit h-fit rounded-l-none border-l-0 py-2 pr-[3px] pl-[1px]',
+                  'transition-visibility hover:bg-sidebar-accent hover:text-sidebar-accent-foreground'
+                )}
+                onClick={togglePin}
+              >
+                {pinned ? (
+                  <SidebarClose data-testid="closeed-pin" />
+                ) : (
+                  <SidebarOpen data-testid="opened-pin" />
+                )}
+              </Button>
+            )}
           </div>
         </div>
       </nav>
